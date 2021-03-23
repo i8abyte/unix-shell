082436485af5f25d4791fecfa8ac60952c497eee
imran-salim <imransalim08@gmail.com>
2021-03-23 22:16:03 +1300

    add readline method

--- main.c
+++ main.c
@@ -19,6 +19,22 @@ void ush_loop(void) {
     } while (status);
 }
 
+char *ush_read_line(void) {
+    char *line = NULL;
+    ssize_t bufsize = 0;
+
+    if (getline(&line, &bufsize, stdin) == -1) {
+        if (feof(stdin)) {
+            exit(EXIT_SUCCESS); // EOF character was recieved
+        } else {
+            perror("readline");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    return line;
+}
+
 int main(int argc, char **argv) {
     ush_loop();
 
